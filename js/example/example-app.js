8861a977fb7917fd2ed902a79ca4345536bd9c8b
balachandarg-tw <balachandar.g@thoughtworks.com>
2025-09-29 13:48:22 +0530

    [INJIMOB-3558]: Remove js folder from release
    Signed-off-by: balachandarg-tw <balachandar.g@thoughtworks.com>

--- js/example/example-app.js
+++
@@ -1,5 +0,0 @@
-const {VCRenderer} = require("../lib/commonJs/vcrenderer.js")
-const {InsuranceVC, MosipVCWithQR, MosipVCWithoutQR} = require('./sample-vc.js')
-VCRenderer.renderSVG(MosipVCWithoutQR).then(
-    (response) => console.log("::::::::::::::SVG Image->",response)
-);
