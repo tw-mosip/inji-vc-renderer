0a1b76359f5915ce8c2767e97831a0ecb98a8c35
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2026-07-03 19:36:24 +0530

    refactor: update pixelpass dependency to 1.0.0-alpha.1-SNAPSHOT
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- kotlin/settings.gradle.kts
+++ kotlin/settings.gradle.kts
@@ -16,6 +16,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
         maven {
             url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
         }
